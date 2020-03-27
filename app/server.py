bc74744a3762d9da7a779a97a27e2e1f546348d4
pStoney <48844449+pStoney@users.noreply.github.com>
2020-03-28 00:28:10 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/12vBMolBwsEOIKuH6SOUcH0SgFRw2yxPf/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=12vBMolBwsEOIKuH6SOUcH0SgFRw2yxPf'
 export_file_name = 'export1.pkl'
 
 classes = ['koalas', 'pandas']
